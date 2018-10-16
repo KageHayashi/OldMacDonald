a9df4c0b68d6895104d84fd4d0a830978d7d98dd
KageHayashi <xiritokun@gmail.com>
2018-10-16 14:32:47 -0700

    L

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,11 @@
-class NamedCow
-{
-
+class NamedCow extends Cow {
+  String name;
+  public NamedCow(String type, String cowname, String sound) {
+    name = cowname;
+    myType = type;
+    mySound = sound;;
+  }
+  public String getName() {
+    return name;
+  }
 }
